25d40fb3d891113a155ff497d55a4e87406a5397
Daniel Fernandes <mooper60@gmail.com>
2019-03-21 21:25:13 -0400

    code cleanup and docs

--- index.js
+++ index.js
@@ -1,49 +1,54 @@
 const Commando = require("discord.js-commando");
 const bot = new Commando.Client();
-const {prefix, token, linkRegex} = require('./config.json') 
-const config = require('./config.json');
+const {token} = require('./config.json') 
 
 const textChannelBlacklist = ["rules", "general", "monstercat-album-art"];
 const radioMap = {};
 
-bot.on('message', (message) => {
-    //collect new radio
-});
 
-
+/** 
+ * Listener method for Discord bot's 'ready' lifecycle event. 
+ * On ready, fetches all messages in bot's guild and constructs 
+ * a map of radio text channels to youtube links in that text channel. 
+ * Also registers commando.js commands under ./cmd/commands dir
+ */
 bot.on('ready', async () => {
     
-    let textChannels = bot.guilds.first().channels.filter(channel => channel.type === "text");
+    let textChannels = bot.guilds.first().channels
+        .filter(channel => channel.type === "text");
 
-    const daPromise = Promise.all(textChannels
+    const constructRadioMapPromise = Promise.all(textChannels
         .filter(textChannel => !textChannelBlacklist.includes(textChannel.name))
         .map(textChannel =>  textChannel.fetchMessages())
     ).then(allMessages => {
         allMessages.forEach(channelMessages => {
-            let linkCollection = collectLinks(channelMessages);
-            let channelName = channelMessages.array()[1].channel.name;
+            let messageArray = channelMessages.array();
+            let linkCollection = collectLinks(messageArray);
+            let channelName = messageArray[0].channel.name;
             if(linkCollection) {
                 radioMap[channelName] = linkCollection;
             }
         })
     });
 
-    await daPromise;
+    await constructRadioMapPromise;
     bot.registry.registerGroup("cmd", "Commands");
     bot.registry.registerDefaults();
     bot.registry.registerCommandsIn(__dirname + '/commands');   
 
-    console.log("ready");
+    console.log("bot ready");
 });
 
 bot.login(token);
 
-
-function collectLinks(messages) {
+/**
+ * Given an array containing discord.js Messages, harvests links
+ * and returns an array of links.
+ * @param {Message[]} messageArray
+ */
+function collectLinks(messageArray) {
     let linkCollection = [];
     let linkRegex = /https:\/\/[a-zA-Z.0-9\/?=&-_]+/g;
-    let messageArray = messages.array();
-    
     for(let i = 0; i < messageArray.length; i++) {
         let message = messageArray[i].content;
         let links = message.match(linkRegex);
@@ -51,7 +56,6 @@ function collectLinks(messages) {
             linkCollection = linkCollection.concat(links);
         }
     }
-
     return linkCollection;
 }
 
