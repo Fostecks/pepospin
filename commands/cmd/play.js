65dca1fbe22cf7f3382ab3d439366761dd947970
Daniel Fernandes <mooper60@gmail.com>
2019-04-01 23:17:19 -0400

    Pull audio player code into its own class

--- commands/cmd/play.js
+++ commands/cmd/play.js
@@ -1,11 +1,7 @@
 const Commando = require("discord.js-commando");
-const discord = require("discord.js");
-const YTDL = require("ytdl-core");
 const indexExports = require("../../index.js");
+const Player = require("../../player.js");
 
-const videoMetadata = {};
-let videoMetadataPromise;
-const promiseMethods = {};
 
 /** 
  * Bot command to make discord bot start streaming youtube audio
@@ -24,74 +20,24 @@ class Play extends Commando.Command {
         });
     }
 
-    /** 
-     * Plays a youtube link as an audio stream into a voiceConnection. 
-     * Sets bot activity as "Playing <title>"
-     * @param {String} trackID full youtube link as a string
-     * @param {VoiceConnection} connection discord guild VoiceConnection object
-     */
-    async play(trackID, connection) {
-        this.resetVideoMetadataPromise();
-        let ytdl = YTDL(trackID, {quality: "highestaudio"});
-        ytdl.on('info', (videoInfo, videoFormat) => {
-            console.log("Audio encoding: " + videoFormat.audioEncoding);
-            console.log("Audio bitrate: " + videoFormat.audioBitrate);
-            console.log("Audio sample rate: " + videoFormat.audio_sample_rate);
-            console.log("Attempting to play: " + videoInfo.title);
-
-            videoMetadata.videoInfo = videoInfo;
-            videoMetadata.videoFormat = videoFormat;
-            promiseMethods.resolve();
-    
-            indexExports.bot.user.setActivity(videoInfo.title, { type: 'PLAYING' });
-        });
-        try {
-            if(indexExports.bot.audioStreamDispatcher) {
-                indexExports.bot.audioStreamDispatcher.destroy();
-            }
-            let dispatcher = connection.playStream(ytdl);
-            return dispatcher;
-        } catch(err) {
-            console.log(err);
-        }
-    }
-
-    resetVideoMetadataPromise() {
-        videoMetadataPromise = new Promise(resolve => promiseMethods.resolve = resolve);
-    }
-
     /**
      * Method that executes on invocation of command.
      * @param {String} message 
      * @param {String} args 
      */
     async run(message, args) {
-        indexExports.bot.killCommand = false;
+        let player = new Player().getInstance();
+        await player.killActiveQueue();
         let connection = message.guild.voiceConnection;
         let radioMap = indexExports.getMap();
         if(radioMap && connection && args) {
             let linkArray = radioMap[args];
+            await player.playArray(linkArray, connection, message.channel).then(() => {
+                if(indexExports.bot.killCommand === false) {
+                    connection.disconnect();
+                }
+            });
             
-            // Audio play loop
-            for(const link of linkArray) {
-                if(indexExports.bot.killCommand === true) break;
-                indexExports.bot.audioStreamDispatcher = await this.play(link, connection);
-
-                await videoMetadataPromise;
-                let richText = new discord.RichEmbed()
-                    .addField("Now Playing", videoMetadata.videoInfo.title)
-                    .setThumbnail(videoMetadata.videoInfo.thumbnail_url)
-                    .setColor(0xFF0000)
-                    .setFooter(videoMetadata.videoFormat.audioBitrate + "kbps • " + 
-                        videoMetadata.videoFormat.audio_sample_rate / 1000 + " kHz • codec: " + 
-                        videoMetadata.videoFormat.audioEncoding);
-
-                message.doNotDelete = true;
-                message.channel.send(richText);
-
-                await new Promise(resolve => indexExports.bot.audioStreamDispatcher.on('end', resolve));
-            }
-            connection.disconnect();
         }
     }
 }
---
+++ player.js
@@ -0,0 +1,114 @@
+const discord = require("discord.js");
+const YTDL = require("ytdl-core");
+const indexExports = require("./index.js");
+
+const _videoMetadata = {};
+let _videoMetadataPromise;
+const _promiseMethods = {};
+
+class Player {
+
+    constructor() { 
+    }
+
+    _resetVideoMetadataPromise() {
+        _videoMetadataPromise = new Promise(resolve => _promiseMethods.resolve = resolve);
+    }
+
+    async _postNowPlaying(channel) {
+        await _videoMetadataPromise;
+        let richText = new discord.RichEmbed()
+            .addField("Now Playing", _videoMetadata.videoInfo.title)
+            .setThumbnail(_videoMetadata.videoInfo.thumbnail_url)
+            .setColor(0xFF0000)
+            .setFooter(_videoMetadata.videoFormat.audioBitrate + "kbps • " + 
+                _videoMetadata.videoFormat.audio_sample_rate / 1000 + " kHz • codec: " + 
+                _videoMetadata.videoFormat.audioEncoding);
+        channel.send(richText);
+    }
+
+    /** 
+     * Plays a youtube link as an audio stream into a voiceConnection. 
+     * Sets bot activity as "Playing <title>"
+     * @param {String} trackID full youtube link as a string
+     * @param {VoiceConnection} connection discord guild VoiceConnection object
+     */
+    async _play(trackID, connection) {
+        this._resetVideoMetadataPromise();
+        let ytdl = YTDL(trackID, {quality: "highestaudio"});
+        ytdl.on('info', (videoInfo, videoFormat) => {
+            console.log("Audio encoding: " + videoFormat.audioEncoding);
+            console.log("Audio bitrate: " + videoFormat.audioBitrate);
+            console.log("Audio sample rate: " + videoFormat.audio_sample_rate);
+            console.log("Attempting to play: " + videoInfo.title);
+
+            _videoMetadata.videoInfo = videoInfo;
+            _videoMetadata.videoFormat = videoFormat;
+            _promiseMethods.resolve(_videoMetadata);
+    
+            indexExports.bot.user.setActivity(videoInfo.title, { type: 'PLAYING' });
+        });
+        try {
+            if(indexExports.bot.audioStreamDispatcher) {
+                indexExports.bot.audioStreamDispatcher.destroy();
+            }
+            let dispatcher = connection.playStream(ytdl);
+            return dispatcher;
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+
+    /**
+     * Plays a queue of youtube links as an audio stream into a voiceConnection.
+     * @param {String[]} linkArray array of links to play in a queue 
+     * @param {VoiceConnection} connection target guild VoiceConnection to stream
+     * @param {TextChannel} channel target channel to post messages with video metadata
+     */
+    async playArray(linkArray, connection, channel) {
+        this._activeQueue = new Promise(async (queueResolve, queueReject) => {
+            // Audio play loop
+            for(const link of linkArray) {
+                if(indexExports.bot.killCommand === true) {
+                    break;
+                }
+                indexExports.bot.audioStreamDispatcher = await this._play(link, connection);
+                this._postNowPlaying(channel);
+                await new Promise(resolve => indexExports.bot.audioStreamDispatcher.on('end', resolve));
+            }
+            queueResolve();
+        });
+        return this._activeQueue;
+    }
+
+    async killActiveQueue() {
+        if(indexExports.bot.audioStreamDispatcher &&
+            !indexExports.bot.audioStreamDispatcher.destroyed) {
+                indexExports.bot.killCommand = true;
+                indexExports.bot.audioStreamDispatcher.destroy();
+        }
+        try {
+            await this._activeQueue;
+        }
+        catch(e) {
+            console.log(e);
+        }
+        indexExports.bot.killCommand = false;
+    }
+}
+
+class Singleton {
+
+    constructor() {
+        if (!Singleton.instance) {
+            Singleton.instance = new Player();
+        }
+    }
+
+    getInstance() {
+        return Singleton.instance;
+    }
+}
+
+module.exports = Singleton;
